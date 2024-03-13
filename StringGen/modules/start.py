573dda6eecd08d0a814d495b5ec2ae67c0fdeba0
Toxiiiic26 <104536820+Toxiiiic26@users.noreply.github.com>
2024-03-13 03:38:59 +0200

    Add files via upload

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -11,7 +11,7 @@ from StringGen.utils import add_served_user, keyboard
 async def f_start(_, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/cdbf07e687a7de3ef012e.jpg",
-        caption=f"""𝐒𝐨𝐮𝐑𝐜𝐞𝐓𝐨𝐱𝐢𝐂\n╮⦿ مرحباً بك عزيزي : {message.from_user.first_name},\n╯⦿  اسمي : {Anony.mention},\n╮⦿ تم صنعي من قبل فيجا\n╯⦿ اعمل علي استخراج جلسات""",
+        caption=f"""𝐒𝐨𝐮𝐑𝐜𝐞𝐓𝐨𝐱𝐢𝐂\n╮⦿ مرحباً بك عزيزي : {message.from_user.first_name},\n╯⦿  اسمي : {Anony.mention},\n╮⦿ تم صنعي من قبل توكسي\n╯⦿ اعمل علي استخراج جلسات""",
         reply_markup=keyboard,
     )
     await add_served_user(message.from_user.id)
